659220ecaa86e7ac85ca6fb71d59a0b2453bdce2
klaus <klaus.bareis@a1.net>
2018-11-06 18:02:16 +0100

    commit-test3

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class Test {
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
 }
