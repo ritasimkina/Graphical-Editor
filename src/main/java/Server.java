1ec0f2f08bb4c945cd0369d1db5b310aa7875a66
Bareis Klaus <a01501513@unet.univie.ac.at>
2018-11-07 14:50:34 +0100

    interface and class generated

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,4 +1,4 @@
-public class Test {
+public class Server {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
