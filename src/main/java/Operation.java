f877dd4e3fab5a024dbd062fd7f3439566f0df33
Margaryta Simkina <a01446530@unet.univie.ac.at>
2018-11-18 23:29:07 +0100

    Replace Operation.java

--- src/main/java/Operation.java
+++ src/main/java/Operation.java
@@ -1,1 +1,12 @@
-public interface Operation {}
+public interface Operation {
+	
+//	public ArrayList<Shape> getAllObjects();
+	
+//	public boolean OperationEdit(Shape s) throws Exception;
+//	public boolean OperationGet(String id);
+//	public boolean OperationAdd(Shape s) throws Exception;
+//	public boolean dOperationDelete(String od) throws Exception;
+//	public boolean OperationMove(String id, Double xMove, Double yMove) throws Exception;
+//	
+//	public String OperationMakeSVG(Shape s) throws Exception;
+}
