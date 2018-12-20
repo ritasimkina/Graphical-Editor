b63ec2372700155450bf13f24d1cdc0ece94135c
Bareis Klaus <a01501513@unet.univie.ac.at>
2018-12-20 23:43:20 +0100

    Elipse: adapted and simplified

--- src/main/java/Screen/DrawArray/Draws/Elipse.java
+++ src/main/java/Screen/DrawArray/Draws/Elipse.java
@@ -1,32 +1,57 @@
 package Screen.DrawArray.Draws;
 
 import Component.IComponent;
+import Screen.DrawArray.Color;
 import Screen.DrawArray.Draw;
 import Screen.DrawArray.Point;
+import Tools.SVGAttributeList;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Elipse  extends Draw {
-    private double rx, ry;
-    private int stroke_width;
-
+    static List<String> ATTRIBUTES= Arrays.asList(
+            // ATTRIBUTES
+            "class", "svg_ellipse",
+            "transform", "",
+            "cx", "50",
+            "cy", "50",
+            "rx", "40",
+            "ry", "20", // "style", "externalResourcesRequired";    style uses as PRESENTATION_ATTRIBUTES
+            //CONDITIONAL_PROCESSING_ATTRIBUTES
+            //"requiredExtensions", "requiredFeatures",  "systemLanguage";
+            //CORE_ATTRIBUTES
+            "id", "id",       //, "xml:base", "xml:lang",  "xml:space";
+            //GRAPHICAL_EVENT_ATTRIBUTES
+            "onclick", "clicked_svg(this.id)",  //,"onactivate",  "onfocusin", "onfocusout", "onload", "onmousedown", "onmousemove", "onmouseout", "onmouseover",  "onmouseup";
+            //PRESENTATION_ATTRIBUTES
+            //Color and Painting properties:
+            "fill", "rgb(255,255,255)",
+            "stroke", "rgb(0,255,255)",
+            "stroke-width", "2"     //, "color-interpolation", "color-interpolation-filters", "color-profile", "color-rendering", "fill-opacity", "fill-rule", "image-rendering", "marker", "marker-end", "marker-mid", "marker-start", "shape-rendering", "stroke-dasharray", "stroke-dashoffset", "stroke-linecap", "stroke-linejoin", "stroke-miterlimit", "stroke-opacity", "text-rendering",
+            //PRESENTATION ATTRIBUTES
+    );
 
     public Elipse(double x, double y, double rx, double ry) {
-        addPoint(new Point(x,y));
-        this.rx=rx;
-        this.ry=ry;
-        addColor(255,0,0);  // fill
-        addColor(0,0,0);    // stroke
-        stroke_width=2;
+        super(new SVGAttributeList(ATTRIBUTES));
+        attributes.setValue("id",get_id());
+        attributes.setValue("cx",x);
+        attributes.setValue("cy",y);
+        attributes.setValue("rx",rx);
+        attributes.setValue("ry",ry);
     }
 
-    public String get_html() {
-        String s;
-        s= "\t<ellipse "+
-                get_id_tag()+
-                " cx='" +points.get(0).getX() + "' cy='" +points.get(0).getY() +
-                "' rx='" +rx+ "' ry='" +ry+
-                "' style='fill:"+color.get(0).gethtml(is_clicked())+
-                ";stroke:"+color.get(1).gethtml(is_clicked())+
-                ";stroke-width:" +stroke_width+ "'"+get_onclick()+" />\n";
-        return s;
+    @Override
+    public SVGAttributeList make_clicked(SVGAttributeList al)    {
+        Color fill=new Color(attributes.getValue("fill"));
+        Color stroke=new Color(attributes.getValue("stroke"));
+        al.setValue("fill",fill.gethtml(true));
+        al.setValue("stroke",stroke.gethtml(true));
+        return al;
+    }
+
+    @Override
+    public String souround_svg_type(String s) {
+        return "\t<ellipse "+ s+ " />\n";
     }
 }
