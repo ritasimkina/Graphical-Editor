d5c6bbbb5ebed1ae7664b00179023417e7e07bfd
Bareis Klaus <a01501513@unet.univie.ac.at>
2018-12-20 23:47:37 +0100

    Line: adapted and simplified

--- src/main/java/Screen/DrawArray/Draws/Line.java
+++ src/main/java/Screen/DrawArray/Draws/Line.java
@@ -1,27 +1,58 @@
 package Screen.DrawArray.Draws;
 
 import Component.IComponent;
+import Screen.DrawArray.Color;
 import Screen.DrawArray.Draw;
 import Screen.DrawArray.Point;
+import Tools.SVGAttributeList;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Line extends Draw  {
-    int stroke_width;
+    static List<String> ATTRIBUTES= Arrays.asList(
+            // ATTRIBUTES
+            "class", "svg_line",
+            "transform", "",
+            "x1", "50",
+            "y1", "50",
+            "x2", "40",
+            "y2", "20", // "style", "externalResourcesRequired";    style uses as PRESENTATION_ATTRIBUTES
+            //CONDITIONAL_PROCESSING_ATTRIBUTES
+            //"requiredExtensions", "requiredFeatures",  "systemLanguage";
+            //CORE_ATTRIBUTES
+            "id", "id",       //, "xml:base", "xml:lang",  "xml:space";
+            //GRAPHICAL_EVENT_ATTRIBUTES
+            "onclick", "clicked_svg(this.id)",  //,"onactivate",  "onfocusin", "onfocusout", "onload", "onmousedown", "onmousemove", "onmouseout", "onmouseover",  "onmouseup";
+            //PRESENTATION_ATTRIBUTES
+            //Color and Painting properties:
+            "fill", "rgb(255,255,255)",
+            "stroke", "rgb(0,255,255)",
+            "stroke-width", "5"     //, "color-interpolation", "color-interpolation-filters", "color-profile", "color-rendering", "fill-opacity", "fill-rule", "image-rendering", "marker", "marker-end", "marker-mid", "marker-start", "shape-rendering", "stroke-dasharray", "stroke-dashoffset", "stroke-linecap", "stroke-linejoin", "stroke-miterlimit", "stroke-opacity", "text-rendering",
+            //PRESENTATION ATTRIBUTES
+    );
 
     public Line(double x1, double y1, double x2, double y2) {
-        addPoint(new Point(x1,y1));
-        addPoint(new Point(x2,y2));
-        addColor(0,0,0);    // stroke
-        stroke_width=2;
+        super(new SVGAttributeList(ATTRIBUTES));
+        attributes.setValue("id",get_id());
+        attributes.setValue("x1",x1);
+        attributes.setValue("y1",y1);
+        attributes.setValue("x2",x2);
+        attributes.setValue("y2",y2);
     }
 
-    public String get_html() {
-        String s;
-        s= "\t<line "+
-                get_id_tag()+
-                " x1='" +points.get(0).getX() + "' y1='" +points.get(0).getY() +
-                "' x2='" +points.get(1).getX() + "' y2='" +points.get(1).getY() +
-                "' style='stroke:"+color.get(0).gethtml(is_clicked())+
-                ";stroke-width:" +stroke_width+ "'"+get_onclick()+" />\n";
-        return s;
+    @Override
+    public SVGAttributeList make_clicked(SVGAttributeList al)    {
+        Color fill=new Color(attributes.getValue("fill"));
+        Color stroke=new Color(attributes.getValue("stroke"));
+        al.setValue("fill",fill.gethtml(true));
+        al.setValue("stroke",stroke.gethtml(true));
+        return al;
     }
+
+    @Override
+    public String souround_svg_type(String s) {
+        return "\t<line "+ s+ " />\n";
+    }
+
 }
