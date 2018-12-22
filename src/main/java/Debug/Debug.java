147fa31bc32148789b76216a7f99361e8f224bed
Bareis Klaus <a01501513@unet.univie.ac.at>
2018-12-22 09:23:49 +0100

    Debug.signal: new

--- src/main/java/Debug/Debug.java
+++ src/main/java/Debug/Debug.java
@@ -12,7 +12,6 @@ public class Debug {
     public static void out(Thread thread, String string)   {
         out(thread, string,insert_minus+1);
     }
-
     private static void out(Thread thread, String string,int level)   {
         final StackTraceElement[] ste = thread.getStackTrace();
         //Set<Thread> threads = Thread.getAllStackTraces().keySet();
@@ -26,7 +25,26 @@ public class Debug {
         if(string.length()>0) s+="\t\t--> "+string;
 
         //System.out.println( s );
-
     }
+
+
+
+    public static void signal(Thread thread)   {
+        signal(thread,"",insert_minus+1);
+    }
+    public static void signal(Thread thread, String string)   {
+        signal(thread, string,insert_minus+1);
+    }
+    private static void signal(Thread thread, String string,int level)   {
+        final StackTraceElement[] ste = thread.getStackTrace();
+        String s="";
+        s+=ste[level].getLineNumber()+": ";
+        s+=thread.getName()+"\t";
+        s+=ste[level].getClassName()+"::";
+        s+=ste[level].getMethodName()+"()";
+        if(string.length()>0) s+="\t\t--> "+string;
+        System.out.println( s );
+    }
+
 
 }
