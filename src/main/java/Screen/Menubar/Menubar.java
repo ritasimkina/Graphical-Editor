a80bf72a525aad4c15437074e68f682941f04d76
Fabian Schmon <a01568351@unet.univie.ac.at>
2018-12-29 16:06:02 +0100

    added top and footer description

--- src/main/java/Screen/Menubar/Menubar.java
+++ src/main/java/Screen/Menubar/Menubar.java
@@ -8,7 +8,7 @@ import Observer.Observer;
 public class Menubar extends Component {
 
     public String get_html()   { Debug.out(Thread.currentThread());
-        String s= "\n<div id=\"menu\"> Screen.Screen.Menubar.Menu</div>\n\n";
+        String s= "\n<h1><div id=\"menu\" style=\"color:white\">&ensp; SE2 Forms</div></h1>\n\n";
         return s;
     }
 }
