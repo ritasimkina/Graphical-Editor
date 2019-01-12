a98b5b825010eb8d23456613904d8d2f9d3cbf7d
Margaryta Simkina <a01446530@unet.univie.ac.at>
2019-01-12 16:43:50 +0200

    Toolbar extended

--- src/main/java/Screen/Toolbar/Toolbar.java
+++ src/main/java/Screen/Toolbar/Toolbar.java
@@ -36,9 +36,6 @@ public class Toolbar extends Component {
         }
     }
 
-
-
-
     public String get_html() {
         Debug.out(Thread.currentThread());
         String s = "";
@@ -46,11 +43,29 @@ public class Toolbar extends Component {
         s+="<div id=\"toolbar\">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;\n";
         s+="\t<table>";
         for (String i : toolbar_names) {
-            s += "\n\t<form action=\"add_svg." + i + "\" method=\"GET\">";
+            s += "\n\t<form class=\"toolbar_form\" action=\"add_svg." + i + "\" original=\"add_svg." + i + "\"  method=\"GET\">";
             s += "\n\t\t<button style=\"height:30px; width:75px\">" + i + "</button>&emsp;&emsp;";
             s += "\n\t</form>";
         }
         s+="\n\t</table>\n";
+        s+="<div style=\"background: #aacae4; padding: 10px;\">\n" +
+            "        Line color: <input type=\"color\" onchange=\"changeColor(this.value);\">\n" +
+            "    </div>\n" +
+            "    <script>\n" +
+            "        function changeColor(color) {\n" +
+            "            console.log(color);\n" +
+            "            var list = document.getElementsByClassName(\"toolbar_form\");\n" +
+            "            console.log(list);\n" +
+            "            for(var i = 0; i < list.length; i++) {\n" +
+            "                list[i].setAttribute(\"action\", list[i].getAttribute(\"original\") + \".color:\" + hexToRgb(color) + \";\")\n" +
+            "            }\n" +
+            "        }\n" +
+            "        \n" +
+            "        function hexToRgb(hex) {\n" +
+            "            var result = /^#?([a-f\\d]{2})([a-f\\d]{2})([a-f\\d]{2})$/i.exec(hex);\n" +
+            "            return \"rgb(\" + parseInt(result[1], 16) + \",\" + parseInt(result[2], 16) + \",\" + parseInt(result[3], 16) + \")\";\n" +
+            "        }\n" +
+            "    </script>";
         s+="</div>\n\n";
         return s;
     }
