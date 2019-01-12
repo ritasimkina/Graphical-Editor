53f6650853da8178780d45f2fa0fae6253961739
Margaryta Simkina <a01446530@unet.univie.ac.at>
2019-01-12 16:42:57 +0200

    Added ColorDraw class decorator

---
+++ src/main/java/Screen/DrawArray/ColorDraw.java
@@ -0,0 +1,23 @@
+package Screen.DrawArray;
+
+import Tools.SVGAttributeList;
+
+public class ColorDraw extends Draw {
+    private final Draw draw;
+
+    public ColorDraw(Draw draw, String color) {
+        super(draw.attributes);
+        draw.attributes.setValue("stroke", color);
+        this.draw = draw;
+    }
+
+    @Override
+    public SVGAttributeList make_clicked(SVGAttributeList al)    {
+        return draw.make_clicked(al);
+    }
+
+    @Override
+    public String souround_svg_type(String s) {
+        return draw.souround_svg_type(s);
+    }
+}
