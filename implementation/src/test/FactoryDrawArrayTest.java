beed53311bcd892132b10ba5b9ad36a23eb98019
Margaryta Simkina <a01446530@unet.univie.ac.at>
2019-01-14 21:34:26 +0100

    Factory Draw Test

---
+++ implementation/src/test/FactoryDrawArrayTest.java
@@ -0,0 +1,23 @@
+//package Factory;
+
+import Screen.DrawArray.DrawArray;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FactoryDrawArrayTest {
+    private FactoryDrawArray factoryDrawArray;
+
+    @Before
+    public void setUp() {
+        this.factoryDrawArray = new FactoryDrawArray();
+    }
+
+    @Test
+    public void ITestFactoryDrawArray() {
+        DrawArray drawArray = (DrawArray) this.factoryDrawArray.create();
+
+        Assert.assertNotNull(drawArray);
+    }
+
+}
